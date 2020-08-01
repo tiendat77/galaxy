1d65c4dc65e5c129c132a045e24a3050f445a7e7
Ji Woo <huynhztienzdat@gmail.com>
2020-08-01 14:55:06 +0700

    feat(effect): typewriter

--- src/app/modules/effects/effects.module.ts
+++ src/app/modules/effects/effects.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Libraries
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+// Components
 import { EffectsRoutingModule } from './effects-routing.module';
 import { MagicButtonComponent } from './magic-button/magic-button.component';
 import { TypeWriterComponent } from './type-writer/type-writer.component';
@@ -12,7 +16,8 @@ import { TypeWriterComponent } from './type-writer/type-writer.component';
   ],
   imports: [
     CommonModule,
-    EffectsRoutingModule
+    EffectsRoutingModule,
+    FlexLayoutModule
   ]
 })
 export class EffectsModule { }
