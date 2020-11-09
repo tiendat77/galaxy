fc16a12ac17dcd139e1a78544d279a0c2f4248bc
Ji Woo <huynhztienzdat@gmail.com>
2020-11-09 17:25:48 +0700

    develop: epoch timestamp converter

--- src/app/modules/tools/epoch-converter/epoch-converter.component.ts
+++ src/app/modules/tools/epoch-converter/epoch-converter.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { NotifyService } from '../../services/notify.service';
+
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-epoch-converter',
@@ -7,9 +12,106 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EpochConverterComponent implements OnInit {
 
-  constructor() { }
+  // ecclock
+  ecclock = moment().unix();
+
+  // timestamp to human date
+  intervalClock;
+  timestampCtrl: FormControl = new FormControl();
+  parsedHumanDate = {
+    utc: undefined,
+    local: undefined,
+  };
+
+  // human date to timestamp
+  humanDateForm: FormGroup;
+  parsedEpochTimestamp: string;
+
+  constructor(
+    private formbuilder: FormBuilder,
+    private notify: NotifyService
+  ) { }
 
   ngOnInit(): void {
+    this.initEcClock();
+    this.initHumanDate2TimestampForm();
+  }
+
+  initEcClock() {
+    this.intervalClock = setInterval(() => {
+      this.ecclock = moment().unix();
+    }, 1000);
+  }
+
+  initHumanDate2TimestampForm() {
+    const now = moment();
+
+    this.humanDateForm = this.formbuilder.group({
+      year: [now.year(), [Validators.required, Validators.min(1970)]],
+      month: [now.month(), []],
+      date: [now.date()],
+      hour: [now.hour()],
+      minute: [now.minute()],
+      second: [now.second()],
+      meridiem: ['AM'],
+      timezone: ['']
+    });
+  }
+
+  // ecclock
+  onClockHover() {
+    if (this.intervalClock) {
+      clearInterval(this.intervalClock);
+    }
+  }
+
+  epoch2human() {
+    const format = 'dddd, MMMM M, YYYY HH:mm:ss A';
+    let humanDateUtc = 'Invalid timestamp';
+    let humanDateLocal = '';
+
+    try {
+      const unixTimestamp = Number.parseInt(this.timestampCtrl.value, 10);
+
+      if (Number.isNaN(unixTimestamp)) {
+        throw Error('NaN');
+      }
+
+      const momentObj = moment.unix(unixTimestamp);
+
+      if (!momentObj.isValid()) {
+        throw Error('Invalid moment');
+      }
+
+      humanDateUtc = momentObj.utc().format(format);
+      humanDateLocal = momentObj.local().format(format + ' (Z)');
+
+    } catch (error) {
+      console.error(error);
+    }
+
+    this.parsedHumanDate.utc = humanDateUtc;
+    this.parsedHumanDate.local = humanDateLocal;
+  }
+
+  human2epoch() {
+    const options = this.humanDateForm.value;
+    for (const key of Object.keys(options)) {
+      const option = options[key];
+      if (option === undefined || option === null || option.length === 0) {
+        this.parsedEpochTimestamp = 'Invalid form';
+        return;
+      }
+    }
+
+    const momentObj = moment().set({
+      year: options.year,
+      month: options.month,
+      date: options.date,
+      hour: options.hour,
+      minute: options.minute,
+      second: options.second,
+    });
   }
 
 }
