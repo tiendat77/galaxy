46d263a25f42b0028937631ff3178583eb0e7402
Ji Woo <huynhztienzdat@gmail.com>
2021-04-06 15:43:04 +0700

    feat(dashboard): magic scroll

--- src/app/core/pages/dashboard/dashboard.component.ts
+++ src/app/core/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+
+// https://scrollmagic.io/examples/basic/reveal_on_scroll.html
+import * as ScrollMagic from 'scrollmagic';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,11 +9,28 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    const controller = new ScrollMagic.Controller();
+    // build scenes
+    let revealElements = document.getElementsByClassName('digit');
+    for (let i = 0; i < revealElements.length; i++) { // create a scene for each element
+      const scene = new ScrollMagic.Scene({
+        triggerElement: revealElements[i], // y value not modified, so we can use element as trigger as well
+        offset: 50, // start a little later
+        triggerHook: 0.9,
+      })
+        .setClassToggle(revealElements[i], 'visible') // add class toggle
+        .addIndicators({ name: 'digit ' + (i + 1) }); // add indicators (requires plugin)
+
+      controller.addScene(scene);
+    }
   }
 
 }
