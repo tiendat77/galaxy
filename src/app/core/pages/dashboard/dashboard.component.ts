ba1b7b234f40dc0a6805dc35fb5624620f153bf1
Ji Woo <huynhztienzdat@gmail.com>
2021-04-14 21:34:28 +0700

    feat: dashboard

--- src/app/core/pages/dashboard/dashboard.component.ts
+++ src/app/core/pages/dashboard/dashboard.component.ts
@@ -1,7 +1,7 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 // https://scrollmagic.io/examples/basic/reveal_on_scroll.html
-import { ScrollMagic, Controller, Scene } from 'scrollmagic';
+import * as ScrollMagic from 'scrollmagic';
 
 // https://github.com/inorganik/CountUp.js
 import { CountUp } from 'countup.js';
@@ -14,6 +14,8 @@ import { CountUp } from 'countup.js';
 })
 export class DashboardComponent implements OnInit, AfterViewInit {
 
+  @ViewChild('scrollTrigger') scrollTrigger: ElementRef;
+
   constructor() { }
 
   ngOnInit(): void {
@@ -25,15 +27,32 @@ export class DashboardComponent implements OnInit, AfterViewInit {
   }
 
   scrollMagic() {
-    const controller = new Controller();
+    const that = this;
+    const trigger = this.scrollTrigger?.nativeElement;
+
+    if (!trigger) {
+      return;
+    }
+
+    const controller = new ScrollMagic.Controller();
+    const scene = new ScrollMagic.Scene({triggerElement: trigger, duration: 200});
+
+    scene.addTo(controller)
+      .on('enter', (e) => {
+        console.log(e);
+        that.countUp();
+      });
   }
 
   countUp() {
-    let demo = new CountUp('myTargetElement', 6701);
-    if (!demo.error) {
-      demo.start();
-    } else {
-      console.error(demo.error);
+    const emoji1 = new CountUp('dashboardEmoji1', 6701);
+    if (emoji1 && !emoji1.error) {
+      emoji1.start();
+    }
+
+    const emoji2 = new CountUp('dashboardEmoji2', 1234);
+    if (emoji2 && !emoji2.error) {
+      emoji2.start();
     }
   }
 
