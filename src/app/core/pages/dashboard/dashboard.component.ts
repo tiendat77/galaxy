4a173d83fd0135103491bf321826e754f939d866
Ji Woo <huynhztienzdat@gmail.com>
2021-04-18 00:23:01 +0700

    feat: dashboard
    - fix container css
    - dashboard now is welcome page

--- src/app/core/pages/dashboard/dashboard.component.ts
+++ src/app/core/pages/dashboard/dashboard.component.ts
@@ -1,64 +1,15 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-
-// https://scrollmagic.io/examples/basic/reveal_on_scroll.html
-import * as ScrollMagic from 'scrollmagic';
-
-// https://github.com/inorganik/CountUp.js
-import { CountUp } from 'countup.js';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  styleUrls: ['./dashboard.component.scss']
 })
-export class DashboardComponent implements OnInit, AfterViewInit {
-
-  @ViewChild('scrollTrigger') scrollTrigger: ElementRef;
-
-  stats = [
-    { id: 'dashboardStats1', value: 6071 },
-    { id: 'dashboardStats2', value: 9600 },
-    { id: 'dashboardStats3', value: 115200 },
-    { id: 'dashboardStats4', value: 2000 },
-  ];
+export class DashboardComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
-  }
-
-  ngAfterViewInit() {
-    this.scrollMagic();
-    this.countUp();
-  }
-
-  scrollMagic() {
-    const that = this;
-    const trigger = this.scrollTrigger?.nativeElement;
-
-    if (!trigger) {
-      return;
-    }
-
-    const controller = new ScrollMagic.Controller();
-    const scene = new ScrollMagic.Scene({triggerElement: trigger, duration: 200});
-
-    scene.addTo(controller)
-      .on('enter', (e) => {
-        console.log(e);
-        that.countUp();
-      });
-  }
-
-  countUp() {
-    this.stats.forEach((stat) => {
-      const counter = new CountUp(stat.id, stat.value);
-
-      if (counter && !counter.error) {
-        counter.start();
-      }
-    });
   }
 
 }
