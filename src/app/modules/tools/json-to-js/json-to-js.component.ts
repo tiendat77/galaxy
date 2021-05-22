5dec8db28ce954de1c496867e06998a1753c9cec
Ji Woo <huynhztienzdat@gmail.com>
2021-05-22 10:12:24 +0700

    feat(tool): js to json

--- src/app/modules/tools/json-to-js/json-to-js.component.ts
+++ src/app/modules/tools/json-to-js/json-to-js.component.ts
@@ -1,24 +1,53 @@
-import { Component, ElementRef, ViewChild } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 import { stringify } from 'javascript-stringify';
 import { NotifyService } from '@app/core/services/notify.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-json-to-js',
   templateUrl: './json-to-js.component.html',
   styleUrls: ['./json-to-js.component.scss']
 })
-export class JsonToJsComponent {
+export class JsonToJsComponent implements OnInit {
 
   @ViewChild('outputRef', { static: false }) private outputRef: ElementRef;
 
-  input = `{\n  "sender": {\n    "id": "USER_ID"\n  }\n}`;
+  /**
+   * mode == true: Json to Js
+   * mode == false: Js to Json
+   */
+  mode = true;
+
+  input = ''; /** `{\n  "sender": {\n    "id": "USER_ID"\n  }\n}` */
   output = '';
   error = '';
 
-  constructor(private notify: NotifyService) { }
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private notify: NotifyService
+  ) { }
 
-  public convert() {
+  ngOnInit() {
+    const mode = this.route.snapshot.paramMap.get('mode');
+
+    if (mode === 'js') {
+      this.mode = false;
+    } else {
+      this.mode = true;
+    }
+  }
+
+  public toggle() {
+    this.mode = !this.mode;
+    this.error = '';
+    this.input = '';
+    this.output = '';
+    this.updateParam();
+  }
+
+  public js() {
     this.error = '';
     this.output = '';
 
@@ -34,6 +63,37 @@ export class JsonToJsComponent {
       }
 
       // what to do?
+      this.error = error.toString();
+    }
+  }
+
+  public json() {
+    this.error = '';
+    this.output = '';
+
+    /**
+     * Thanks to sKumarBinay
+     * Github: https://github.com/sKumarBinay/convert-string-to-object
+     */
+    function convertToObject(data) {
+      const json = data.replace("export default", "").replace(/(\r\n|\n|\r)/gm, "").replace(/ /g, "").replace(/"/g, "").replace(/'/g, "").replace(/{/g, '{"').replace(/}/g, '"}').replace(/:/g, '":"').replace(/,/g, '","').replace(/:"{/g, ":{").replace(/}"}/g, "}}").replace(/}","{/g, "},{").replace(/]"}/g, "]}").replace(/:"\[/g, ':["').replace(/]",/g, "],").replace(/],/g, '"],').replace(/]}/g, '"]}').replace(/\(/g, '("').replace(/\)/g, '")').replace(/"{/g, "{").replace(/}"/g, "}");
+      const obj = JSON.parse(json);
+
+      Object.keys(obj).forEach(k => {
+        const int = parseInt(obj[k]);
+        isNaN(int) || "object" == typeof obj[k] || (obj[k] = int);
+      });
+
+      return obj;
+    }
+
+    try {
+      const jsObject = convertToObject(this.input);
+      const json = JSON.stringify(jsObject, null, 2);
+      this.output = json;
+      this.scrollToOutput();
+
+    } catch (error) {
       this.error = error.toString();
     }
   }
@@ -64,9 +124,14 @@ export class JsonToJsComponent {
     }, 250);
   }
 
+  private updateParam() {
+    const mode = this.mode ? 'json' : 'js';
+    this.router.navigate(['/tools/json-to-js', {mode}]);
+  }
+
   private scrollToOutput() {
     setTimeout(() => {
-      const element = this.outputRef.nativeElement;
+      const element = this.outputRef?.nativeElement;
       element?.scrollIntoView({
         behavior: 'smooth'
       });
