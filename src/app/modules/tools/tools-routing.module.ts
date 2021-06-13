30e0748676e8e1ef8c815579d8ffab29e920c0ce
Ji Woo <huynhztienzdat@gmail.com>
2021-06-13 17:41:22 +0700

    init: flex-layout tool

--- src/app/modules/tools/tools-routing.module.ts
+++ src/app/modules/tools/tools-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 
 /** Components */
 import { JsonToJsComponent } from './json-to-js/json-to-js.component';
+import { FlexLayoutComponent } from './flex-layout/flex-layout.component';
 import { UnixConverterComponent } from './unix-converter/unix-converter.component';
 
 const routes: Routes = [
   { path: 'json-to-js', component: JsonToJsComponent },
+  { path: 'flex-layout', component: FlexLayoutComponent },
   { path: 'unix-converter', component: UnixConverterComponent }
 ];
 
