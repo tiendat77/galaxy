7c5d1a043b5739606f878cfe7058c80fa7ed7bb6
Ji Woo <huynhztienzdat@gmail.com>
2021-07-11 13:17:10 +0700

    feat(tools): box-shadow collection

--- src/app/modules/tools/box-shadow/box-shadows.ts
+++ src/app/modules/tools/box-shadow/box-shadows.ts
@@ -1,11 +1,33 @@
-export interface BoxShadow {
-  name: string;
-  shadow: string;
-}
-
-export const BOX_SHADOWS: BoxShadow[] = [
-  {
-    name: 'example',
-    shadow: 'rgba(149, 157, 165, 0.2) 0px 8px 24px'
-  }
+export const BOX_SHADOWS: string[] = [
+  'rgba(149, 157, 165, 0.2) 0px 8px 24px',
+  'rgba(100, 100, 111, 0.2) 0px 7px 29px 0px',
+  'rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px',
+  'rgba(0, 0, 0, 0.35) 0px 5px 15px',
+  'rgba(0, 0, 0, 0.16) 0px 1px 4px',
+  'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+  'rgba(99, 99, 99, 0.2) 0px 2px 8px 0px',
+  'rgba(0, 0, 0, 0.1) 0px 4px 12px',
+  'rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(0, 0, 0, 0.3) 0px 8px 16px -8px',
+  'rgba(50, 50, 93, 0.25) 0px 6px 12px -2px, rgba(0, 0, 0, 0.3) 0px 3px 7px -3px',
+  'rgba(0, 0, 0, 0.19) 0px 10px 20px, rgba(0, 0, 0, 0.23) 0px 6px 6px',
+  'rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px',
+  'rgba(60, 64, 67, 0.3) 0px 1px 2px 0px, rgba(60, 64, 67, 0.15) 0px 1px 3px 1px',
+  'rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+  'rgba(0, 0, 0, 0.2) 0px 18px 50px -10px',
+  'rgba(0, 0, 0, 0.07) 0px 1px 1px, rgba(0, 0, 0, 0.07) 0px 2px 2px, rgba(0, 0, 0, 0.07) 0px 4px 4px, rgba(0, 0, 0, 0.07) 0px 8px 8px, rgba(0, 0, 0, 0.07) 0px 16px 16px',
+  'rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px',
+  'rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px',
+  'rgba(0, 0, 0, 0.2) 0px 12px 28px 0px, rgba(0, 0, 0, 0.1) 0px 2px 4px 0px, rgba(255, 255, 255, 0.05) 0px 0px 0px 1px inset',
+  'rgba(0, 0, 0, 0.15) 0px 5px 15px 0px',
+  'rgba(17, 17, 26, 0.05) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px',
+  'rgba(17, 17, 26, 0.1) 0px 0px 16px',
+  'rgba(17, 17, 26, 0.1) 0px 8px 24px, rgba(17, 17, 26, 0.1) 0px 16px 56px, rgba(17, 17, 26, 0.1) 0px 24px 80px',
+  'rgba(17, 17, 26, 0.1) 0px 4px 16px, rgba(17, 17, 26, 0.05) 0px 8px 32px',
+  'rgba(0, 0, 0, 0.15) 0px 15px 25px, rgba(0, 0, 0, 0.05) 0px 5px 10px',
+  'rgba(0, 0, 0, 0.18) 0px 2px 4px',
+  'rgba(0, 0, 0, 0.2) 0px 18px 24px -7px',
+  'rgba(0, 0, 0, 0.2) 0px 20px 18px',
+  '0 1px 5px rgba(0, 0, 0, 0.46)',
+  '0 0.5px 0 0 #ffffff inset, 0 1px 2px 0 #B3B3B3',
+  '0 0 10px rgba(0, 0, 0, 0.5)',
 ];
